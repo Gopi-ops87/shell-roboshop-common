5bda2f0f094759405987d3066f7239edffe5e7c9
gopi-ops-87 <gopipasham2000@gmal.com>
2026-02-07 11:24:57 +0530

    mongodb

--- common.sh
+++ common.sh
@@ -7,7 +7,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FOLDER="/var/log/shell-roboshop-common"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 START_TIME=$(date +%s)
