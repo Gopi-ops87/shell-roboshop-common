23b6572eb0940152a13b9fa9beb1b16d267bb580
gopi-ops-87 <gopipasham2000@gmal.com>
2026-02-09 10:22:21 +0530

    redis

--- redis.sh
+++ redis.sh
@@ -10,10 +10,8 @@ dnf module enable redis:7 -y  &>>$LOG_FILE
 VALIDATE $? "enabling redis 7"
 dnf install redis -y   &>>$LOG_FILE
 VALIDATE $? "Installing redis"
-sed -i \
- -e 's/^bind 127.0.0.1/bind 0.0.0.0/' \
-  -e 's/^protected-mode yes/protected-mode no/' \
-  /etc/redis/redis.conf &>>$LOG_FILE
+
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf &>>$LOG_FILE
 VALIDATE $? "allowing remote connections"
 systemctl enable redis  &>>$LOG_FILE
 VALIDATE $? "enabling redis"
