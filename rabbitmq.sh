924f4cad3244194df4928c8626c330974812088f
gopi-ops-87 <gopipasham2000@gmal.com>
2026-02-09 11:17:05 +0530

    rabbitmq

---
+++ rabbitmq.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+source ./common.sh
+
+root_check
+
+cp $SCRIPT_DIR/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo &>>$LOG_FILE
+VALIDATE $? "Adding raabitmq repo"
+dnf install rabbitmq-server -y &>>$LOG_FILE
+VALIDATE $? "installing rabbitmq"
+systemctl enable rabbitmq-server &>>$LOG_FILE
+VALIDATE $? "enabling rabbitmq"
+systemctl start rabbitmq-server &>>$LOG_FILE
+VALIDATE $? "starting rabbitmq"
+rabbitmqctl add_user roboshop roboshop123 &>>$LOG_FILE
+VALIDATE $? "adding user"
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$LOG_FILE
+VALIDATE $? "setting permissions"
+
+print_total_time
