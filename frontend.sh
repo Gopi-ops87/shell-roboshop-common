ce8787ba98aa48bb76f9c0c58ccf18aafbe2e998
gopi-ops-87 <gopipasham2000@gmal.com>
2026-02-09 12:27:30 +0530

    frontend

--- frontend.sh
+++ frontend.sh
@@ -1,7 +1,7 @@
+
 #!/bin/bash
 
-USER_ID=$(id -u)
-
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
@@ -10,53 +10,44 @@ N="\e[0m"
 LOGS_FOLDER="/var/log/shell-roboshop-common"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 SCRIPT_DIR=$PWD
-MONGODB_HOST="mongodb.dev28p.online"
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"  # /var/log/shell-roboshop/12-cart.log
+MONGODB_HOST=mongodb.dev28p.online
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 
-mkdir -p "$LOGS_FOLDER"
-START_TIME=$(date +%s)
-echo "script started executed at: $(date)" | tee -a $LOG_FILE
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
-if [ $USER_ID -ne 0 ]; then
-    echo "ERROR:: please use root access"
-    exit 1  # failure is other than 0
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
 fi
 
-
-VALIDATE() {   #function to receive inputs through args just like shell script args
-            if [ $1 -ne 0 ]; then
-                echo -e "$2 ....$R failure $N" | tee -a &>>$LOG_FILE
-                exit 1
-            else
-                echo -e "$2.. $G  success $N" | tee -a &>>$LOG_FILE
-            fi
+VALIDATE(){ # functions receive inputs through args just like shell script args
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
+    fi
 }
 
 dnf module disable nginx -y &>>$LOG_FILE
-VALIDATE $? "disabling nginx"
 dnf module enable nginx:1.24 -y &>>$LOG_FILE
-VALIDATE $? "Enabling nginx"
 dnf install nginx -y &>>$LOG_FILE
-VALIDATE $? "Installing NGINX"
+VALIDATE $? "Installing Nginx"
 
 systemctl enable nginx  &>>$LOG_FILE
-systemctl start nginx &>>$LOG_FILE
-VALIDATE $? "Starting NGINX"
+systemctl start nginx 
+VALIDATE $? "Starting Nginx"
 
-rm -rf /usr/share/nginx/html/*  
+rm -rf /usr/share/nginx/html/* 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOG_FILE
-cd /usr/share/nginx/html &>>$LOG_FILE
+cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip &>>$LOG_FILE
-VALIDATE $? "Download frontend"
+VALIDATE $? "Downloading frontend"
 
-rm -rf /etc/nginx/nginx.conf 
-cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf &>>$LOG_FILE
+rm -rf /etc/nginx/nginx.conf
+cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
 VALIDATE $? "Copying nginx.conf"
 
-systemctl restart nginx &>>$LOG_FILE
-VALIDATE $? "Restarting ngnx"
-
-END_TIME=$(date +%s)
-TOTAL_TIME=$((END_TIME - START_TIME))
-
-echo -e "Total execution time is: $Y $TOTAL_TIME seconds $N"
+systemctl restart nginx 
+VALIDATE $? "Restarting Nginx"
