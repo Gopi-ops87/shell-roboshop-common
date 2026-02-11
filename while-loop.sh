4885cffcfd87eac85ddc9029985391c3e02ade62
gopi-ops-87 <gopipasham2000@gmal.com>
2026-02-11 11:23:28 +0530

    while

--- while-loop.sh
+++ while-loop.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
-count=$1
+#count=$1
 
-while [ $count -gt 0 ]
-do 
-  echo "Time left: $count"
-  sleep 1
-  count=$((count -1))
-done
+#while [ $count -gt 0 ]
+#do 
+  #echo "Time left: $count"
+  #sleep 1
+  #count=$((count -1))
+#done
 
-echo "Times up"
+#echo "Times up"
+
+
+
+while IFS= read -r line; do
+
+    #process each line here
+    echo "Processing line: $line"
+    #example: you can perform other operation with the file variable
+    #for instance;
+    #some_command "$line"
+    #if [[ "$line" == "specific-syntax" ]]; then
+    #echo "found specific text"
+    #fi
+done < user.sh
