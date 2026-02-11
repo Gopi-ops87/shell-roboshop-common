f4d66c3fab8a922fe224e85ae0fa3b21198b9b87
gopi-ops-87 <gopipasham2000@gmal.com>
2026-02-11 11:56:15 +0530

    backup

--- backup.sh
+++ backup.sh
@@ -9,6 +9,8 @@ N="\e[0m"
 LOGS_FOLDER="/var/log/shell-roboshop-common"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SOURCE_DIR=$1
+DEST_DIR=$2
 
 mkdir -p $LOGS_FOLDER
 
@@ -20,10 +22,20 @@ if [ $USERID -ne 0 ]; then
 fi
 
 USAGE(){
-    echo "USAGE:: sudo sh backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS>"
+    echo -e "$R USAGE:: sudo sh backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS> $N"
     exit 1;
 }
 
 if [ $# -lt 2 ]; then
     USAGE
+fi
+
+if [ ! -d $SOURCE_DIR ]; then
+    echo -e "$R ERROR:: Source directory $SOURCE_DIR doesn't exist $N"
+    exit 1
+fi
+
+if [ ! -d $DEST_DIR ]; then
+    echo -e "$R ERROR:: Destnation directory $DEST_DIR doesn't exist $N"
+    exit 1
 fi
