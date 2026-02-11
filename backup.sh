aca8ca3dc062557d89241f39f9fe45309863c03e
gopi-ops-87 <gopipasham2000@gmal.com>
2026-02-11 12:20:13 +0530

    backup

--- backup.sh
+++ backup.sh
@@ -8,6 +8,7 @@ N="\e[0m"
 
 SOURCE_DIR=$1
 DEST_DIR=$2
+DAYS=${3:-14}  #if not provided as 14 days
 LOGS_FOLDER="/var/log/shell-roboshop-common"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
@@ -40,3 +41,16 @@ if [ ! -d $DEST_DIR ]; then
     echo -e "$R ERROR:: Destnation directory $DEST_DIR doesn't exist $N"
     exit 1
 fi
+
+FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime $DAYS)
+
+if [ ! -z "${FILES}" ]; then
+    echo "Files found: $FILES"
+    TIMESTAMP=$(date +%F-%H-%M)
+    ZIP_FILENAME="$DEST_DIR/app-logs-$TIMESTAMP.zip"
+    echo "zip file name: $ZIP_FILENAME"
+    echo find $SOURCE_DIR -name "*.log" -type f -mtime $DAYS | zip -@ -j "$ZIP_FILENAME"
+else
+    echo -e "No files to archive .... $Y sipping $N"
+fi 
+
